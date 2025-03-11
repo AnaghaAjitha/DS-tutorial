40a868a01b20f14d8b782f9e9bf54943b7784495
AnaghaAjitha <168955434+AnaghaAjitha@users.noreply.github.com>
2025-03-11 19:28:10 +0530

    Update tutorial1.py

--- Tutorial 1/tutorial1.py
+++ Tutorial 1/tutorial1.py
@@ -3,25 +3,27 @@ import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error,r2_score
 import numpy as np
 
-data=pd.read_csv('Advertising.csv')
+data=pd.read_csv('Advertising.csv') #reading dataset using panda
 
+#define predictor variables
 tv=data['TV']
 radio=data['Radio']
 newspaper=data['Newspaper']
 y=data['Sales']
 
-
+#function to calculate model statistics for each feature
 def calc_values(x,y,feature):#x-predictors;y-target
-    x=sm.add_constant(x)
-    model=sm.OLS(y,x).fit()
-    prediction=model.predict(x)
+    x=sm.add_constant(x)#adding intercept to predictor variable using statsmodels.api
+    model=sm.OLS(y,x).fit()#creating an ols model to reduce errors between actual and predicted value;
+    #.fits()train the model on given data
+    prediction=model.predict(x) #predict sales using the ols model
 
     #rse
-    mse=mean_squared_error(y,prediction)
-    rse=np.sqrt(mse)
+    mse=mean_squared_error(y,prediction)#computing mse to find rse using sklearn.metrics.mean_squared_error
+    rse=np.sqrt(mse)#rse is sq root of mse using numpy for square root calculation
 
     #r2
-    r2 = r2_score(y,prediction)
+    r2 = r2_score(y,prediction)#sklearn.metrics.r2_score
 
     #f-statistics
     f_stat = model.f_pvalue
@@ -32,7 +34,8 @@ def calc_values(x,y,feature):#x-predictors;y-target
     print(f"F-statistic: {f_stat}\n")
     
     return rse,r2,f_stat
-
+    
+#call calac_value function to print result of impact of advertising using different medium
 calc_values(tv,y,"TV")
 calc_values(radio,y,"Radio")
 calc_values(newspaper,y,"Newspaper")
